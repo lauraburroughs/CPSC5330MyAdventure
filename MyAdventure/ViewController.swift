7370aaef6eb31d235dc21c18c9fda2f315b352b2
Laura Burroughs <hellolauraburroughs@gmail.com>
2026-02-01 10:44:40 -0600

    Added the first node

--- MyAdventure/ViewController.swift
+++ MyAdventure/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  MyAdventure
-//
+//  CPSC 5330 Assignment 5
 //  Created by Laura Burroughs on 2/1/26.
 //
 
@@ -13,28 +13,43 @@ struct Choice {
     let destinationIndex: Int
 }
 
-
-
-
-
+struct StoryNode {
+    let text: String
+    let choices: [Choice]
+    let isEnding: Bool
+    let isSuccess: Bool
+}
 
 
 
 // Controller
 class ViewController: UIViewController {
-
+    
+    // Array of all story nodes
+    var storyNodes: [StoryNode] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Array of choices
+        let firstChoices = [
+            Choice(text: "Go left", destinationIndex: 0),
+            Choice(text: "Go right", destinationIndex: 1),
+        ]
+
+
+        // First storyNode
+        let startNode = StoryNode(
+            text: "Oh no! You took a wrong turn on your nature hike and find yourself lost in a haunted forest! Which path will you take?",
+            choices: firstChoices,
+            isEnding: false,
+            isSuccess: false
+        )
+        
+        
+        // Add the first node
+        storyNodes.append(startNode)
+
     }
-
-    
-    
-    
-    
-    
-    
-    
-
 }
 
